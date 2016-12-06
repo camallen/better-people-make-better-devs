089af9c46f0ded464ba69c53bb8eaa2654cd0015
Campbell Allen <campbell.allen@gmail.com>
2016-12-06 14:27:31 +0000

    jeopardy style board for zoominar

--- js/scripts.js
+++ js/scripts.js
@@ -6,46 +6,44 @@ $(function(){
         dataType:'json',
         url:'board.json',
         success:function(data){
-            map = data;
-            loadBoard();
+          map = data;
+          loadBoard();
+        },
+        error:function(error, msg){
+          console.log(msg);
         }
     });
     $('.unanswered').click(function(){
-        var category = $(this).parent().data('category');
-        var question = $(this).data('question');
-        var value = map[category].questions[question].value;
-        var answers = $('#answers');
-        $('.modal-title').empty().text(map[category].name);
-        $('#question').empty().text(map[category].questions[question].question);
-        answers.empty();
-        $.each(map[category].questions[question].answers, function(i, answer){
-            answers.append(
-                '<button class="btn btn-danger answer" ' +
-                    'data-category="'+category+'"' +
-                    'data-question="'+question+'"' +
-                    'data-value="'+value+'"' +
-                    'data-correct="'+answer.correct+'"' +
-                    '>'+ answer.text+'</button><br><br>'
-            )
-        });
-        $('#question-modal').modal('show');
-        console.log(category, question);
-        console.log(map[category].questions[question]);
-        handleAnswer();
+      var clicked = $(this);
+      var category = clicked.parent().data('category');
+      var topic = clicked.data('topic');
+      var value = map[category].topics[topic].value;
+      var modalHeader = map[category].name + ' - ' + map[category].topics[topic].value;
+      $('.modal-title').empty().text(modalHeader);
+      //keep the footer empty
+      var topicContent = $('#topic');
+      topicContent.empty();
+      $.each(map[category].topics[topic].notes, function(i, note){
+          topicContent.append('<ul><li>' + note + '</li></ul>')
+      });
+      $('#topic-modal').modal('show');
+      $('#topic-modal').on('hidden.bs.modal', function () {
+        // clicked.unbind();
+        clicked.addClass("disabled");
+      });
+      updateScore(clicked);
     });
-
 });
 var score = 0;
 var map;
 function loadBoard(){
     var board = $('#main-board');
-    var columns = map.length;
-    var column_width = parseInt(12/columns);
-    console.log(columns);
+    var numColumns = map.length;
+    var column_width = parseInt(12/numColumns);
     $.each(map, function(i,category){
         //load category name
         var header_class = 'text-center col-md-' + column_width;
-        if (i === 0 && columns % 2 != 0){
+        if (i === 0 && numColumns % 2 != 0){
             header_class += ' col-md-offset-1';
         }
         $('.panel-heading').append(
@@ -53,32 +51,22 @@ function loadBoard(){
         );
         //add column
         var div_class = 'category col-md-' + column_width;
-        if (i === 0 && columns % 2 != 0){
+        if (i === 0 && numColumns % 2 != 0){
             div_class += ' col-md-offset-1';
         }
         board.append('<div class="'+div_class+'" id="cat-'+i+'" data-category="'+i+'"></div>');
         var column = $('#cat-'+i);
-        $.each(category.questions, function(n,question){
-            //add questions
-            column.append('<div class="well question unanswered" data-question="'+n+'">'+question.value+'</div>')
+        $.each(category.topics, function(n,topic){
+          //add questions
+          column.append('<div class="well topic unanswered" data-topic="'+n+'">'+topic.value+'</div>')
         });
     });
     $('.panel-heading').append('<div class="clearfix"></div>')
 
 }
-
-function updateScore(){
+function updateScore(clicked){
+  if (!clicked.hasClass('disabled')) {
+    score += 1;
     $('#score').empty().text(score);
-}
-
-function handleAnswer(){
-    $('.answer').click(function(){
-        var tile= $('div[data-category="'+$(this).data('category')+'"]>[data-question="'+$(this).data('question')+'"]')[0];
-        $(tile).empty().removeClass('unanswered').unbind().css('cursor','not-allowed');
-        if ($(this).data('correct')){
-            score += parseInt($(this).data('value'));
-        }
-        $('#question-modal').modal('hide');
-        updateScore();
-    })
+  }
 }
